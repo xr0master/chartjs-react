10dff859b0428ecd06d32f9d7959c0332cff8902
Sergey Khomushin <xr0master@gmail.com>
2024-12-25 15:48:02 +0800

    setupTests: clean non-used code

--- setupTests.ts
+++ setupTests.ts
@@ -1,22 +1,9 @@
-import { jest, expect } from '@jest/globals';
+import { expect } from '@jest/globals';
 import '@testing-library/jest-dom';
 import { TestingLibraryMatchers } from '@testing-library/jest-dom/types/matchers-standalone';
 
 declare module '@jest/expect' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface Matchers<R>
     extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
 }
-
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
